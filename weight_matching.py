414f11c1bf7d23bba709be535c9e6dd3a531a592
ogkalu2 <115248977+ogkalu2@users.noreply.github.com>
2022-11-18 18:45:30 -0400

    Update weight_matching.py

--- weight_matching.py
+++ weight_matching.py
@@ -790,13 +790,13 @@ def weight_matching(ps: PermutationSpec, params_a, params_b, max_iter=100, init_
       A = torch.zeros((n, n))
       for wk, axis in ps.perm_to_axes[p]:
         w_a = params_a[wk]
-          w = params_b[wk]
-          for axis, p in enumerate(ps.axes_to_perm[wk]):
-           # Skip the axis we're trying to permute.
-           if axis == None:
+        w = params_b[wk]
+        for axis, p in enumerate(ps.axes_to_perm[wk]):
+          # Skip the axis we're trying to permute.
+          if axis == None:
             continue
-           # None indicates that there is no permutation relevant to that axis.
-           if p is not None:
+          # None indicates that there is no permutation relevant to that axis.
+          if p is not None:
             w = torch.index_select(w, axis, perm[p].int())                    
         w_b = w
         w_a = torch.moveaxis(w_a, axis, 0).reshape((n, -1))
