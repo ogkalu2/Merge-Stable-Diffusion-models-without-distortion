bb599a665f4bce0a2291737d756102c16a479414
ogkalu2 <115248977+ogkalu2@users.noreply.github.com>
2022-11-19 15:56:56 -0400

    Update weight_matching.py

--- weight_matching.py
+++ weight_matching.py
@@ -780,7 +780,10 @@ def apply_permutation(ps: PermutationSpec, perm, params):
 
 def weight_matching(ps: PermutationSpec, params_a, params_b, max_iter=100, init_perm=None):
   """Find a permutation of `params_b` to make them match `params_a`."""
-  perm_sizes = {p: params_a[axes[0][0]].shape[axes[0][1]] for p, axes in ps.perm_to_axes.items()}
+  for p, axes in ps.perm_to_axes.items():
+    if p in params_a and axes in params_a:
+      perm_sizes = {p: params_a[axes[0][0]].shape[axes[0][1]]}
+    else: continue
 
   perm = {p: torch.arange(n) for p, n in perm_sizes.items()} if init_perm is None else init_perm
   perm_names = list(perm.keys())
@@ -792,12 +795,13 @@ def weight_matching(ps: PermutationSpec, params_a, params_b, max_iter=100, init_
       n = perm_sizes[p]
       A = torch.zeros((n, n))
       for wk, axis in ps.perm_to_axes[p]:
-        w_a = params_a[wk]                   
-        w_b = get_permuted_param(ps, perm, wk, params_b, except_axis=axis)
-        w_a = torch.moveaxis(w_a, axis, 0).reshape((n, -1))
-        w_b = torch.moveaxis(w_b, axis, 0).reshape((n, -1))
-       
-        A += w_a @ w_b.T
+        if wk in params_a and params_b:
+          w_a = params_a[wk]
+          w_b = get_permuted_param(ps, perm, wk, params_b, except_axis=axis)
+          w_a = torch.moveaxis(w_a, axis, 0).reshape((n, -1))
+          w_b = torch.moveaxis(w_b, axis, 0).reshape((n, -1))    
+          A += w_a @ w_b.T
+        else: continue
 
       ri, ci = linear_sum_assignment(A.detach().numpy(), maximize=True)
       assert (torch.tensor(ri) == torch.arange(len(ri))).all()
