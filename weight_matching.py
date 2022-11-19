3ed9ff0aed18709107b2bc97c79f0c800840cb5f
ogkalu2 <115248977+ogkalu2@users.noreply.github.com>
2022-11-18 20:35:15 -0400

    Update weight_matching.py

--- weight_matching.py
+++ weight_matching.py
@@ -810,8 +810,8 @@ def weight_matching(ps: PermutationSpec, params_a, params_b, max_iter=100, init_
       ri, ci = linear_sum_assignment(A.detach().numpy(), maximize=True)
       assert (torch.tensor(ri) == torch.arange(len(ri))).all()
 
-      oldL = jnp.vdot(A, jnp.eye(n)[perm[p]])
-      newL = jnp.vdot(A, jnp.eye(n)[ci, :])
+      oldL = torch.dot(A, torch.eye(n)[perm[p].long()])
+      newL = torch.dot(A, torch.eye(n)[ci, :])
       if not silent: print(f"{iteration}/{p}: {newL - oldL}")
       progress = progress or newL > oldL + 1e-12
 
