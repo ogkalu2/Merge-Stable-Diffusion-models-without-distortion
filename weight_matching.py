c806304b579c32b9a8c4884bc8c6232254ddb484
ogkalu2 <115248977+ogkalu2@users.noreply.github.com>
2022-11-19 20:36:15 -0400

    Update weight_matching.py

--- weight_matching.py
+++ weight_matching.py
@@ -778,37 +778,27 @@ def apply_permutation(ps: PermutationSpec, perm, params):
   """Apply a `perm` to `params`."""
   return {k: get_permuted_param(ps, perm, k, params) for k in params.keys()}
 
-def weight_matching(ps: PermutationSpec, params_a, params_b, max_iter=100, init_perm=None):
+def weight_matching(ps: PermutationSpec, params_a, params_b, max_iter=20, init_perm=None):
   """Find a permutation of `params_b` to make them match `params_a`."""
-  for p, axes in ps.perm_to_axes.items():
-    if p in params_a:
-      perm_sizes = {p: params_a[axes[0][0]].shape[axes[0][1]]}
-      perm = {p: torch.arange(n) for p, n in perm_sizes.items()} if init_perm is None else init_perm
-      perm_names = list(perm.keys())
-    else: continue
+  perm_sizes = {p: params_a[axes[0][0]].shape[axes[0][1]] for p, axes in ps.perm_to_axes.items()}
+  perm = {p: torch.arange(n) for p, n in perm_sizes.items()} if init_perm is None else init_perm
+  perm_names = list(perm.keys())
 
   for iteration in range(max_iter):
-    progress = False
     for p_ix in torch.randperm(len(perm_names)):
       p = perm_names[p_ix]
       n = perm_sizes[p]
       A = torch.zeros((n, n))
       for wk, axis in ps.perm_to_axes[p]:
-        if wk in params_a and params_b:
           w_a = params_a[wk]
           w_b = get_permuted_param(ps, perm, wk, params_b, except_axis=axis)
           w_a = torch.moveaxis(w_a, axis, 0).reshape((n, -1))
           w_b = torch.moveaxis(w_b, axis, 0).reshape((n, -1))    
           A += w_a @ w_b.T
-        else: continue
 
       ri, ci = linear_sum_assignment(A.detach().numpy(), maximize=True)
       assert (torch.tensor(ri) == torch.arange(len(ri))).all()
-      
-      oldL = torch.vdot(torch.flatten(A), torch.flatten(torch.eye(n)[perm[p].long()]))
-      newL = torch.vdot(torch.flatten(A), torch.flatten(torch.eye(n)[ci, :]))
-      print(f"{iteration}/{p}: {newL - oldL}")
-      progress = progress or newL > oldL + 1e-12
+      print(f"{iteration}/{p})
 
       perm[p] = torch.Tensor(ci)
 
